da97497a08bd52689b2517c0101783c432c01ef0
abhed agarwal <abhed.agl@gmail.com>
2025-03-17 19:52:40 +0530

    edit

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const mongouri = "mongodb://localhost:27017/inotebook"; // Replace 'inotebook' with your database name
+
+const connectToMongo = () => {
+  mongoose.connect(mongouri)
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+      console.error('Error connecting to MongoDB:', err);
+    });
+};
+
+module.exports = connectToMongo;

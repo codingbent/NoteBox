a5213967697f68cbf7d87d05995841c3f27dcb9b
abhed agarwal <abhed.agl@gmail.com>
2025-07-06 13:59:02 +0530

    edit

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,7 @@ const express = require('express')
 connectToMongo();
 
 const app = express()
-const port = 3000
-
+const port = 5001
 app.use(express.json())
 
 app.use('/api/auth',require('./routes/auth'))
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,29 @@
 const express =require('express')
 const router =express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
 //CREATE A USER USING : POST "/API/AUTH" Doesn't require auth
 
 
-router.post('/',(req,res)=>{
-    console.log(req.body)
-    const user=User(req.body)
-    user.save()
-    res.send(req.body);
+router.post('/',[
+    body('name').isLength({min: 3}),
+    body('password').isLength({min: 8}),
+    body('email').isEmail(),
+],(req,res)=>{
+    const errors =validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    } 
+    User.create({
+        name:req.body.name,
+        password:req.body.password,
+        email:req.body.email
+    }).then(user=>res.json(user))
+    .catch(err=>  {console.log(err)
+        res.json({error:'Please enter a valid entry'})
+    });
+    console.log("Successfull Send");
 })
 
 module.exports = router;

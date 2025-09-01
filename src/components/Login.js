c8cc94e5f5bb8762c01801311a97f0dadf374c2e
abhed agarwal <abhed.agl@gmail.com>
2025-09-01 23:17:53 +0530

    debug

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ const Login = (props) => {
         });
     const handleSubmit = async (e)=>{
         e.preventDefault();
-        const host="http://"
+        const host = "https://NoteBox-env.eba-2zkqs3ih.ap-south-1.elasticbeanstalk.com";
         const response = await fetch(`${host}localhost:5002/api/auth/login`,{
             method :"POST" ,
             headers:{

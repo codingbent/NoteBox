8fb12146415f08bf320d16a69bcc9507334b02ad
abhed agarwal <abhed.agl@gmail.com>
2025-09-01 22:13:30 +0530

    Final

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,6 @@ app.use(cors())
 
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
-
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
